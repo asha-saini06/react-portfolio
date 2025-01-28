fcfe1ebe899038d0d0af1e07c3f94897cc5d7572
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2025-01-28 10:58:33 +0000

    Update images & alt text for Portfolio, About, Home components; modify siteProps for new user details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Asha Saini",
+  title: "Frontend Developer",
+  email: "sainiasha.ash@gmail.com",
+  gitHub: "asha-saini06",
+  instagram: "",
+  linkedIn: "ashasaini06",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youtube: "",
 };
 
 const primaryColor = "#4E567E";
